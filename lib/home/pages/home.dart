1ef56d337b6e179e88b6c71ab05dade4b2705bc5
Richard Jansen <richard@pedroja.tech>
2024-02-24 12:57:36 +0100

    translations; fix for new formdata handling; version bump

--- lib/home/pages/home.dart
+++ lib/home/pages/home.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:flutter/foundation.dart';
+// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:logging/logging.dart';
