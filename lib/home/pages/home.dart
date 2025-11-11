5137616276fd3851a2efe0bead3d37e32f54a014
Richard Jansen <demo_epso@proton.me>
2025-11-11 11:19:52 +0100

    commented out test location; version bump

--- lib/home/pages/home.dart
+++ lib/home/pages/home.dart
@@ -202,7 +202,7 @@ class _ShltrAppState extends State<ShltrApp> with SingleTickerProviderStateMixin
     Locale? locale = coreUtils.lang2locale(languageCode);
 
     Widget initialPage;
-    locationUuid = '5512b8e4-eeb1-4a8f-a5b0-bc1d1a735b8e';
+    // locationUuid = '5512b8e4-eeb1-4a8f-a5b0-bc1d1a735b8e';
     // equipmentUuid = 'e8954213-3660-4d4b-8b07-268844e75cdb';
 
     if (isLoggedIn && equipmentUuid != null) {
